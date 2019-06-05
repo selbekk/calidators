7e580e2d36412b26cb73351b1d2353874e7a6820
Ray Knight <array.knight+github@gmail.com>
2019-06-05 10:01:08 -0700

    fix(isGreaterThan, isLessThan): fix decimals

--- src/__tests__/isGreaterThan.spec.js
+++ src/__tests__/isGreaterThan.spec.js
@@ -5,8 +5,12 @@ const message = 'fail';
 
 describe('isGreaterThan validator', () => {
     it('accepts when value is greater than config value', () => {
+        expect(isGreaterThan({ message, value: 0 })('0.001')).toBe(null);
         expect(isGreaterThan({ message, value: 10 })('10.001')).toBe(null);
         expect(isGreaterThan({ message, value: 10 })('11')).toBe(null);
+        expect(isGreaterThan({ message, value: -10 })('0')).toBe(null);
+        expect(isGreaterThan({ message, value: -10 })(20)).toBe(null);
+        expect(isGreaterThan({ message, value: -10 })('a')).toBe(null);
         expect(isGreaterThan({ message, value: -1000 })('-100')).toBe(null);
         expect(isGreaterThan({ message, value: '0' })('1')).toBe(null);
         expect(isGreaterThan({ message, value: 'a' })('b')).toBe(null);
@@ -18,10 +22,13 @@ describe('isGreaterThan validator', () => {
         expect(isGreaterThan({ message, value: 0 })('0')).toBe(message);
         expect(isGreaterThan({ message, value: 1.123 })('1.123')).toBe(message);
         expect(isGreaterThan({ message, value: '0' })('0')).toBe(message);
+        expect(isGreaterThan({ message, value: 17 })(17)).toBe(message);
         expect(isGreaterThan({ message, value: 'a' })('a')).toBe(message);
     });
 
     it('rejects when value is less than config value', () => {
+        expect(isGreaterThan({ message, value: 0 })('-1')).toBe(message);
+        expect(isGreaterThan({ message, value: 0 })('-0.001')).toBe(message);
         expect(isGreaterThan({ message, value: 1.001 })('1')).toBe(message);
         expect(isGreaterThan({ message, value: 11 })('10')).toBe(message);
         expect(isGreaterThan({ message, value: -100 })('-1000')).toBe(message);
